1e3306999a204b909adb509e0370005acb74137b
Pedro Paulo <pedropaulolucas@hotmail.com>
2021-09-07 20:30:30 -0300

    added my name on main page

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Meu primeiro mobile app React Native</Text>
+      <Text style={styles.title}>Meu primeiro mobile app React Native - Pedro Paulo</Text>
       <Button title="Meu primeiro button" />
     </View>
   );
